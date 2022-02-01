670d501af55cce03546be7ed512f385a1962b9b8
wargie <wargie@bk.ru>
2022-02-01 19:12:05 +0200

    Добавлено решение к задаче

--- Example_011_MethodArray/Program.cs
+++ Example_011_MethodArray/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] array = {1, 12, 31, 4, 15, 16, 17, 18,};
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+    while (index < n)
+    {
+        if(array[index] == find)
+        {
+            Console.WriteLine(index);
+        }
+    //index = index + 1
+    index ++;
+    }
