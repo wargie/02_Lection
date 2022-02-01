dbaeeb500dfe44d460ca729782001d2a7e5ffee1
wargie <wargie@bk.ru>
2022-02-01 19:29:54 +0200

    Создан первый метод

---
+++ Example_12_Array_Prod_alpha/Program.cs
@@ -0,0 +1,11 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
